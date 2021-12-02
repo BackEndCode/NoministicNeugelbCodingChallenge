d3e50758aad66c69978298e2f67e3e597bd5cd4f
muhammadnouman <rescue.nomi@gmail.com>
2021-12-02 16:57:55 +0100

    Splash screen added

--- app/src/main/java/com/noministic/neugelbcodingchallenge/SplashActivity.java
+++ app/src/main/java/com/noministic/neugelbcodingchallenge/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.noministic.neugelbcodingchallenge;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        moveToHome();
+    }
+    private void moveToHome(){
+        int SPLASH_DISPLAY_LENGTH = 3000;
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
